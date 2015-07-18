e194c862aa5731efb9545ed6f0fb00c2e9390b0e
DXkite <DXkite@163.com>
2015-07-18 17:58:16 +0800

    优化 2015-7-18

--- SDL2Teach/v3/component/js/common.js
+++ SDL2Teach/v3/component/js/common.js
@@ -1,29 +1,37 @@
 function addCssByLink(url)
 {
 
-    var doc = document;
-    var link = doc.createElement("link");
-    link.setAttribute("rel", "stylesheet");
-    link.setAttribute("type", "text/css");
-    link.setAttribute("href", url);
-    
-    var heads = doc.getElementsByTagName("head");
-    if (heads.length)
-        heads[0].appendChild(link);
-    else
-        doc.documentElement.appendChild(link);
-	//	alert(url);
+	var doc = document;
+	var link = doc.createElement("link");
+	link.setAttribute("rel", "stylesheet");
+	link.setAttribute("type", "text/css");
+	link.setAttribute("href", url);
+
+	var heads = doc.getElementsByTagName("head");
+	if (heads.length)
+		heads[0].appendChild(link);
+	else
+		doc.documentElement.appendChild(link);
+	// alert(url);
 }
 
 function showIMG(url)
 {
-//alert(url);
-event.cancelBubble = true;
-var img_view=document.getElementById("img_view");
-img_view.style.display="block";
-var img_show=document.getElementById("img_show");
-img_show.src=url;
+	// alert(url);
+	event.cancelBubble = true;
+	var img_view = document.getElementById("img_view");
+	img_view.style.display = "block";
+	var img_show = document.getElementById("img_show");
+	img_show.src = url;
 
+}
+
+function call_show(Obj)
+{
+	Obj.onclick = function()
+	{
+		showIMG(Obj.attributes.getNamedItem("src").nodeValue);
+	}
 }
 
 function addImgPop()
@@ -35,63 +43,59 @@ function addImgPop()
 		var show;
 		if (imgs[i].className == "scr_shoot")
 		{
-			show = imgs[i];
-			show.onclick = function()
-			{
-
-				var url = show.attributes.getNamedItem("src").nodeValue;
-				// alert(url);
-				showIMG(url);
-			}
-
+			call_show(imgs[i]);
 		}
-
 	}
 }
 
 function load()
 {
-   // var computer = document.getElementById("computer");
-    //var phone = document.getElementById("phone");
-    var menu = document.getElementById("menu");
-    var ctrl_menu = document.getElementById("ctrl_menu");
+	var loading = document.getElementById("loading");
+	var loaded = document.getElementById("loaded");
+	var menu = document.getElementById("menu");
+	var ctrl_menu = document.getElementById("ctrl_menu");
 
-    if (window.innerWidth < 800)
-    {
-       // phone.style.display = "block";
-        addCssByLink("./component/css/phone-styles.css");
+	if (window.innerWidth < 800)
+	{
+		// phone.style.display = "block";
+		addCssByLink("./component/css/phone-styles.css");
 		addImgPop();
-    }
-    else
-    {
-      //  computer.style.display = "block";
-        addCssByLink("./component/css/pc-styles.css");
-        
-    }
-	
+	}
+	else
+	{
+		// computer.style.display = "block";
+		addCssByLink("./component/css/pc-styles.css");
 
-    ctrl_menu.onclick = function ()
-    {
-        event.cancelBubble = true;
-		//alert(imgs.length);
-        if (menu.style.display == "none")
-        {
-            
-            menu.style.display = "block";
-            
-        }
-        else
-        {
-            menu.style.display = "none";
-            
-        }
-        
-    }
-    window.onclick = function ()
-    {
-        menu.style.display = "none";
-		document.getElementById("img_view").style.display="none";	
-    }
+	}
+
+
+	ctrl_menu.onclick = function()
+	{
+		event.cancelBubble = true;
+		// alert(imgs.length);
+		if (menu.style.display == "none")
+		{
+
+			menu.style.display = "block";
+
+		}
+		else
+		{
+			menu.style.display = "none";
+
+		}
+
+	}
+	window.onclick = function()
+	{
+		menu.style.display = "none";
+		document.getElementById("img_view").style.display = "none";
+	}
+	if (loading&&loaded)
+	{
+	loaded.style.display = "block";
+	loading.style.display ="none";
+	}
 }
 
 window.onload = load;
